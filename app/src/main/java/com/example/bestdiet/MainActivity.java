07947f0f9631858d89e61592565e616485b45320
alekra1 <alekseyuwww12@gmail.com>
2024-02-26 23:18:22 +0200

    Finished starting screen, button leads to login screen

--- app/src/main/java/com/example/bestdiet/MainActivity.java
+++ app/src/main/java/com/example/bestdiet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bestdiet;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -18,12 +19,22 @@ import com.example.bestdiet.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.starting_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, login_screen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
