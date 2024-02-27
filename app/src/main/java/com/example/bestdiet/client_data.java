712f77f384ddb9f3b2bb8814bbca77a3a8808aa4
Makson1212 <ymalyovanyj@gmail.com>
2024-02-27 19:25:06 +0200

    123

---
+++ app/src/main/java/com/example/bestdiet/client_data.java
@@ -0,0 +1,27 @@
+package com.example.bestdiet;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "clients_data")
+public class client_data {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @ColumnInfo(name = "middleName")
+    public String middleName;
+
+    @ColumnInfo(name = "last_name")
+    public String lastName;
+
+    @ColumnInfo(name = "year")
+    public int year;
+
+    @ColumnInfo(name = "gender")
+    public int gender;
+
+}
