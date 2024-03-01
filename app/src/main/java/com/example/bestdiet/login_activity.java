a904d80ec7d502269e025c0451da39da23836dfc
Makson1212 <ymalyovanyj@gmail.com>
2024-03-01 21:28:46 +0200

    для лучшего

--- app/src/main/java/com/example/bestdiet/login_activity.java
+++ app/src/main/java/com/example/bestdiet/login_activity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import java.util.List;
 
 public class login_activity extends AppCompatActivity {
+    public static AppDatabase appDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +20,27 @@ public class login_activity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
 
         Button myButton = findViewById(R.id.start_registr);
+
+        appDatabase = Room.databaseBuilder(getApplicationContext(),
+                        AppDatabase.class, "bestDiet")
+                .build();
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(login_activity.this, login_activitytwo.class);
                 startActivity(intent);
+                /*EditText pib = findViewById(R.id.editTextText2);
+                String pibString = pib.toString();
+                String[] splitpib = pibString.split(" ");
+                EditText email = findViewById(R.id.editTextTextEmailAddress);
+                String emailstring = email.toString();
+                EditText phone = findViewById(R.id.editTextPhone);
+                String phonestring = phone.toString();
+                EditText password = findViewById(R.id.editTextTextPassword);
+                String passwordstring = password.toString();
+                user user = null;
+                user.setUser(splitpib[0],splitpib[1],splitpib[2],emailstring,phonestring,passwordstring);
+                appDatabase.userDao().insert(user);*/
             }
         });
     }
--- app/src/main/java/com/example/bestdiet/user.java
+++ app/src/main/java/com/example/bestdiet/user.java
@@ -24,4 +24,64 @@ public class user {
 
     @ColumnInfo(name = "password")
     public String password;
+
+    public int getUid() {
+        return uid;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUser(String firstName, String middleName, String lastName, String email, String phone, String password) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
 }
