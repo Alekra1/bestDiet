fb009c3ec4dda1d35b28b1f2d7ad6f8f2beded31
Oleksii <alekseyuwww12@gmail.com>
2024-04-07 19:57:03 +0300

    Finished main menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     val room_version = "2.6.1"
-    implementation ("com.github.prolificinteractive:material-calendarview:${version}")
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
 
